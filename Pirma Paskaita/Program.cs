d46a975b6f4cc5f4622cffe4a364d724378a2340
Jodzko <A.Jodzko@gmail.com>
2024-10-07 21:53:55 +0300

    First commit

---
+++ Pirma Paskaita/Program.cs
@@ -0,0 +1,91 @@
+﻿using System.Drawing;
+
+Console.WriteLine ("        *****");
+    Console.WriteLine("    **       **");
+    Console.WriteLine("  **           **");
+    Console.WriteLine("**     Hello     **");
+    Console.WriteLine("**      from     **");
+    Console.WriteLine("  **    C#     **");
+    Console.WriteLine("    **       **");
+    Console.WriteLine("       *****");
+Console.WriteLine("");
+Console.WriteLine("");
+
+
+Console.ForegroundColor
+            = ConsoleColor.White;
+Console.WriteLine("     /\\");
+Console.ForegroundColor
+    = ConsoleColor.Green;
+Console.WriteLine("    /  \\");
+Console.ForegroundColor
+    = ConsoleColor.Red;
+Console.WriteLine("   /    \\");
+Console.ForegroundColor
+    = ConsoleColor.DarkMagenta;
+Console.WriteLine("  /______\\");
+Console.WriteLine("");
+Console.WriteLine("");
+
+Console.ForegroundColor
+            = ConsoleColor.DarkBlue;
+Console.WriteLine("  / \\       / \\     ");
+Console.ForegroundColor
+            = ConsoleColor.Blue;
+Console.WriteLine(" /   \\     /   \\");
+Console.ForegroundColor
+    = ConsoleColor.Cyan;
+Console.WriteLine(" \\    \\   /    /");
+Console.ForegroundColor
+    = ConsoleColor.Yellow;
+Console.WriteLine("  \\           /");
+Console.ForegroundColor
+    = ConsoleColor.DarkYellow;
+Console.WriteLine("   \\         /");
+Console.ForegroundColor
+    = ConsoleColor.DarkMagenta;
+Console.WriteLine("    \\       /");
+Console.ForegroundColor
+    = ConsoleColor.Magenta;
+Console.WriteLine("     \\     /");
+Console.ForegroundColor
+    = ConsoleColor.Red;
+Console.WriteLine("      \\   /");
+Console.ForegroundColor
+    = ConsoleColor.White;
+Console.WriteLine("       \\ /");
+Console.ForegroundColor
+    = ConsoleColor.Green;
+Console.WriteLine("        V");
+Console.WriteLine("");
+Console.WriteLine("");
+
+Console.ForegroundColor
+    = ConsoleColor.White;
+
+Console.WriteLine("");
+Console.WriteLine("");
+Console.WriteLine("");
+
+
+string Preke = "0buolys";
+int kaina = 2;
+int kiekis = 5;
+Console.WriteLine("Preke: " + Preke );
+Console.WriteLine("Kaina: " + kiekis);
+Console.WriteLine("Kaina: " + kaina + " eur");
+Console.WriteLine("");
+Console.WriteLine("");
+
+string v = "Artur Jodzko";
+int a = 29;
+string p = "Software developer";
+string e = "A.Jodzko@gmail.com";
+string n = "+37060645585";
+Console.WriteLine("Vardas: " + v);
+Console.WriteLine("Amzius: " + a);
+Console.WriteLine("Pareigos: " + p);
+Console.WriteLine("E. Pastas: " + e);
+Console.WriteLine("Tel.: " + n);
+
+
